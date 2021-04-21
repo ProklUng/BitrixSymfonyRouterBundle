0cb9eaf850ccdc7926df3420b878317bb04395bf
Fedor <funtik21975@gmail.com>
2021-04-21 22:03:26 +0300

    Доработки.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Prokl\BitrixSymfonyRouterBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package Prokl\BitrixSymfonyRouterBundle\DependencyInjection
+ *
+ * @since 21.04.2021
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('symfony_router');
+        $rootNode    = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->booleanNode('enabled')->defaultValue(true)->end()
+                ->scalarNode('router_request_context_host')->defaultValue('%kernel.site.host%')->end()
+                ->scalarNode('router_request_context_scheme')->defaultValue('http')->end()
+                ->scalarNode('router_request_context_base_url')->defaultValue('')->end()
+                ->scalarNode('router_cache_path')->defaultValue('%kernel.cache_dir%/routes')->end()
+                ->scalarNode('http_port')->defaultValue(80)->end()
+                ->scalarNode('https_port')->defaultValue(443)->end()
+                ->scalarNode('resource')->defaultValue('%kernel.project_dir%/app/routes.yaml')->end()
+                ->booleanNode('utf8')->defaultValue(true)->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
--- DependencyInjection/SymfonyRouterExtension.php
+++ DependencyInjection/SymfonyRouterExtension.php
@@ -3,10 +3,15 @@
 namespace Prokl\BitrixSymfonyRouterBundle\DependencyInjection;
 
 use Exception;
+use Symfony\Bundle\FrameworkBundle\Routing\AnnotatedRouteControllerLoader;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
+use Symfony\Component\Routing\Loader\AnnotationFileLoader;
 
 /**
  * Class SymfonyRouterExtension
@@ -24,15 +29,20 @@ class SymfonyRouterExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container) : void
     {
-        // $configuration = new Configuration();
-        // $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . self::DIR_CONFIG)
         );
 
         $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $this->registerRouterConfiguration(
+            $config,
+            $container
+        );
     }
 
     /**
@@ -41,5 +51,93 @@ class SymfonyRouterExtension extends Extension
     public function getAlias() : string
     {
         return 'symfony_router';
+    }
+
+    /**
+     * А-ля Симфоническая инициализация роутера.
+     *
+     * @param array            $config    Конфигурация.
+     * @param ContainerBuilder $container Контейнер.
+     *
+     * @throws Exception
+     * @return void
+     *
+     * @since 20.11.2020
+     */
+    private function registerRouterConfiguration(
+        array $config,
+        ContainerBuilder $container
+    ): void {
+        $annotationsConfigEnabled = $container->getParameter('enable_annotations');
+
+        if ($config['default_uri'] === null) {
+            $host = $container->getParameter('kernel.http.host');
+            $schema = $container->getParameter('kernel.schema');
+            $config['default_uri'] = $schema . '://' . $host . '/';
+        }
+
+        if (!$this->isConfigEnabled($container, $config)) {
+            $container->removeDefinition('console.command.router_debug');
+            $container->removeDefinition('console.command.router_match');
+            return;
+        }
+
+        if (null === $config['utf8']) {
+            trigger_deprecation(
+                'symfony/framework-bundle',
+                '5.2',
+                'Not setting the "framework.router.utf8" configuration option is deprecated, 
+                it will default to "true" in version 6.0.');
+        }
+
+        $container->setParameter('router.request.context.host', $config['router_request_context_host']);
+        $container->setParameter('router.request.context.scheme', $config['router_request_context_scheme']);
+        $container->setParameter('router.request.context.base_url', $config['router_request_context_base_url']);
+        $container->setParameter('router.cache.path', $config['router_cache_path']);
+        $container->setParameter('default_uri', $config['default_uri']);
+
+        if ($config['utf8']) {
+            $container->getDefinition('routing.loader')->replaceArgument(1, ['utf8' => true]);
+        }
+
+        if (!class_exists(ExpressionLanguage::class)) {
+            $container->removeDefinition('router.expression_language_provider');
+        }
+
+        $container->setParameter('router.resource', $config['resource']);
+        $router = $container->findDefinition('router.default');
+        $argument = $router->getArgument(2);
+        $argument['strict_requirements'] = $config['strict_requirements'];
+        // @phpstan-ignore-next-line
+        if (isset($config['type'])) {
+            $argument['resource_type'] = $config['type'];
+        }
+        $router->replaceArgument(2, $argument);
+
+        $container->setParameter('request_listener.http_port', $config['http_port']);
+        $container->setParameter('request_listener.https_port', $config['https_port']);
+
+        if ($annotationsConfigEnabled) {
+            $container->register('routing.loader.annotation', AnnotatedRouteControllerLoader::class)
+                ->setPublic(false)
+                ->addTag('routing.loader', ['priority' => -10])
+                ->addArgument(new Reference('annotation_reader'));
+
+            $container->register('routing.loader.annotation.directory', AnnotationDirectoryLoader::class)
+                ->setPublic(false)
+                ->addTag('routing.loader', ['priority' => -10])
+                ->setArguments([
+                    new Reference('file_locator'),
+                    new Reference('routing.loader.annotation'),
+                ]);
+
+            $container->register('routing.loader.annotation.file', AnnotationFileLoader::class)
+                ->setPublic(false)
+                ->addTag('routing.loader', ['priority' => -10])
+                ->setArguments([
+                    new Reference('file_locator'),
+                    new Reference('routing.loader.annotation'),
+                ]);
+        }
     }
 }
